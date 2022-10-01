a0b37f32d7d80cde92916ccec34763ae8f148064
zuzexx <tjasa.zilavec@gmail.com>
2022-10-01 15:32:32 +0200

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <Head title="Welcome" />
+  <section>this is welcome</section>
+</template>
+
+<script>
+import { Head } from "@inertiajs/inertia-vue3";
+
+export default {
+  name: "Welcome",
+  components: { Head },
+};
+</script>
