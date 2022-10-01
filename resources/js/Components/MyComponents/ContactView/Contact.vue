5fb9f65ca8a6656d4bd780225ac56f4e9b346f1c
zuzexx <tjasa.zilavec@gmail.com>
2022-10-01 15:41:05 +0200

    .started contact page

---
+++ resources/js/Components/MyComponents/ContactView/Contact.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>this is contact</section>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+};
+</script>
+
+<style></style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,13 +1,16 @@
 <template>
   <Head title="Welcome" />
-  <section>this is welcome</section>
+  <section>
+    <Contact />
+  </section>
 </template>
 
 <script>
 import { Head } from "@inertiajs/inertia-vue3";
+import Contact from "../Components/MyComponents/ContactView/Contact.vue";
 
 export default {
   name: "Welcome",
-  components: { Head },
+  components: { Head, Contact },
 };
 </script>
